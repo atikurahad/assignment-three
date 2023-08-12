dd0d86c3d5291ef5684881a9b50997dd50d364c1
atikurahad <atikurahad@gmail.com>
2023-08-12 13:16:33 +0600

    task 3 complete

--- index.js
+++ index.js
@@ -11,47 +11,49 @@
 //     if(Object.keys(properties).length === 0){
 //         console.log("Object is empty")
 //     }
-    
+
 //     for (const property in properties) {
-        
+
 //          console.log(`${property } : ${properties[property]}`)
 
-        
 //     }
 
 // }
 
 // printObjectProperties(person)
 
-    
-
 // 2) javascript function returns
 
- function findMaxNumber (params){
-    
-     if(params.length === 0){
-        return null
-     }
+// function findMaxNumber(params) {
+//   if (params.length === 0) {
+//     return null;
+//   }
 
-     const maxNum = Math.max(...params)
-        return maxNum
-     
+//   const maxNum = Math.max(...params);
+//   return maxNum;
+// }
+// const numbers1 = [10, 20, 5, 30, 25];
 
- }
- const numbers1 = [10, 20, 5, 30, 25];
+// console.log(findMaxNumber(numbers1)); // Output: 30
 
- console.log(findMaxNumber(numbers1)); // Output: 30
+// const numbers2 = [5, 15, 7];
 
- const numbers2 = [5, 15, 7];
+// console.log(findMaxNumber(numbers2)); // Output: 15
+
+// const emptyArray = [];
+
+// console.log(findMaxNumber(emptyArray)); // Output: null
+
+// 3) javascript Spread Operator
+
+  function mergeArrays (array1 , array2){
 
 
- console.log(findMaxNumber(numbers2)); // Output: 15
- 
- 
-  
- 
- 
- const emptyArray = [];
- 
- 
- console.log(findMaxNumber(emptyArray)); // Output: null
+    let newArr = [...array1, ...array2]
+    return newArr
+  }
+  const arr1 = [1, 2, 3];
+
+const arr2 = [4, 5, 6];
+
+console.log(mergeArrays(arr1,arr2));

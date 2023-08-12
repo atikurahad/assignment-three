049977ca5a81fc3c473c60f8b049067460c6eb08
atikurahad <atikurahad@gmail.com>
2023-08-12 12:25:11 +0600

    first task complete

--- index.js
+++ index.js
@@ -1,2 +1,51 @@
-// 1) javascript For in loop
+// // 1) javascript For in loop
 
+let person ={
+    // name: "Shihab",
+    // age: 28,
+    // gender: "male",
+    // occupation:"Job holder",
+}
+
+const printObjectProperties = (properties)=>{
+    if(properties.keys === 0){
+        console.log("Object is empty")
+    }
+    
+    for (const property in properties) {
+        
+         console.log(`${property } : ${properties[property]}`)
+
+        
+    }
+
+}
+
+printObjectProperties(person)
+
+// function printObjectProperties(obj){
+
+//     if(Object.keys(obj).length===0){
+//         console.log('Object is empty');
+//         return
+//     }
+    
+//       for(property in obj){
+//         console.log(`${property}:${obj[property]}`);
+    
+//     }
+    
+//     }
+    
+//     const person={
+//         name: "Leo Messi",
+//         age:36,
+//         prof: "Footballer"
+//     }
+//     const pp={
+    
+//     }
+    
+//     printObjectProperties(person);
+    
+    

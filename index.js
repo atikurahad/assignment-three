49ec6703d3e1d04149ac7d02416694b7ea339568
atikurahad <atikurahad@gmail.com>
2023-08-12 14:18:15 +0600

    final submission

--- index.js
+++ index.js
@@ -1,92 +1,159 @@
-// 1) javascript For in loop
+//1) javascript For in loop
 
-// const person ={
-//     name: "Shihab",
-//     age: 28,
-//     gender: "Male",
-//     occupation:"Job holder",
-// }
+const person = {
+  name: "Shihab",
+  age: 28,
+  gender: "Male",
+  occupation: "Job holder",
+};
 
-// const printObjectProperties = (properties)=>{
-//     if(Object.keys(properties).length === 0){
-//         console.log("Object is empty")
-//     }
+const printObjectProperties = (properties) => {
+  if (Object.keys(properties).length === 0) {
+    console.log("Object is empty");
+  }
 
-//     for (const property in properties) {
+  for (const property in properties) {
+    console.log(`${property} : ${properties[property]}`);
+  }
+};
 
-//          console.log(`${property } : ${properties[property]}`)
+printObjectProperties(person);
 
-//     }
+//2) javascript function returns
 
-// }
+function findMaxNumber(params) {
+  if (params.length === 0) {
+    return null;
+  }
 
-// printObjectProperties(person)
+  const maxNum = Math.max(...params);
+  return maxNum;
+}
+const numbers1 = [10, 20, 5, 30, 25];
 
-// 2) javascript function returns
+console.log(findMaxNumber(numbers1)); // Output: 30
 
-// function findMaxNumber(params) {
-//   if (params.length === 0) {
-//     return null;
-//   }
+const numbers2 = [5, 15, 7];
 
-//   const maxNum = Math.max(...params);
-//   return maxNum;
-// }
-// const numbers1 = [10, 20, 5, 30, 25];
+console.log(findMaxNumber(numbers2)); // Output: 15
 
-// console.log(findMaxNumber(numbers1)); // Output: 30
+const emptyArray = [];
 
-// const numbers2 = [5, 15, 7];
+console.log(findMaxNumber(emptyArray)); // Output: null
 
-// console.log(findMaxNumber(numbers2)); // Output: 15
+//3) javascript Spread Operator
 
-// const emptyArray = [];
+function mergeArrays(array1, array2) {
+  let newArr = [...array1, ...array2];
+  return newArr;
+}
+const arr1 = [1, 2, 3];
 
-// console.log(findMaxNumber(emptyArray)); // Output: null
+const arr2 = [4, 5, 6];
 
-// 3) javascript Spread Operator
+console.log(mergeArrays(arr1, arr2));
 
-//   function mergeArrays (array1 , array2){
+//4) javascript arrow function
 
-//     let newArr = [...array1, ...array2]
-//     return newArr
-//   }
-//   const arr1 = [1, 2, 3];
+const calculateSquare = (number) => {
+  const square = number * number;
+  return square;
+};
 
-// const arr2 = [4, 5, 6];
+console.log(calculateSquare(5)); // Output: 25
 
-// console.log(mergeArrays(arr1,arr2));
+console.log(calculateSquare(8)); // Output: 64
 
-// 4) javascript arrow function
+//5) javascript es6 map
 
-// const calculateSquare = (number) =>{
+function doubleNumbers(makeDouble) {
+  const result = makeDouble.map((number) => number * 2);
+  return result;
+}
+const numbers = [1, 2, 3, 4, 5];
 
-//     const square = number * number;
-//    return square;
-// }
+console.log(doubleNumbers(numbers)); // Output: [2, 4, 6, 8, 10]
 
-// console.log(calculateSquare(5)); // Output: 25
+// 6) javascript es6 static keyword
 
-// console.log(calculateSquare(8)); // Output: 64
 
-// 5) javascript es6 map
-
-// function doubleNumbers(makeDouble) {
-//   const result = makeDouble.map(number => number * 2);
-//   return result;
-// }
-// const numbers = [1, 2, 3, 4, 5];
-
-// console.log(doubleNumbers(numbers)); // Output: [2, 4, 6, 8, 10]
-
-class MathUtility{
-    static multiply(num1, num2){
-         const result = num1 * num2
-         return result;
-    }
+class MathUtility {
+  static multiply(num1, num2) {
+    const result = num1 * num2;
+    return result;
+  }
 }
 
 console.log(MathUtility.multiply(5, 3)); // Output: 15
 
+console.log(MathUtility.multiply(2, 8)); // Output: 16
 
-console.log(MathUtility.multiply(2, 8)); // Output: 16git 
+
+// 7) javascript es6 class inheritance
+
+ 
+
+class Animal {
+  constructor(name, sound) {
+    this.name = name;
+    this.sound = sound;
+  }
+
+  makeSound() {
+    console.log(`${this.name} makes a sound: ${this.sound}`);
+  }
+}
+
+class Dog extends Animal {
+  constructor(name, sound) {
+    super(name, sound);
+  }
+
+  fetch() {
+    console.log(`${this.name} is fetching the ball!`);
+  }
+}
+
+const animal = new Animal("Lion", "Roar");
+
+animal.makeSound(); // Output: "Roar"
+
+const dog = new Dog("Buddy", "Woof");
+
+dog.makeSound();
+
+dog.fetch();
+
+
+//8) javascript es6 super keyword
+
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  introduce() {
+    console.log(`Hi, I'm ${this.name} and I'm ${this.age} years old.`);
+  }
+}
+
+class Student extends Person {
+  constructor(name, age, major) {
+    super(name, age);
+    this.major = major;
+  }
+
+  introduce() {
+    console.log(
+      `Hi, I'm ${this.name}, I'm ${this.age} years old, and I'm majoring in ${this.major}.`
+    );
+  }
+}
+
+// Creating instances and using methods
+const person1 = new Person("Sakib", 34);
+person1.introduce();
+
+const student1 = new Student("Tamim", 36, "Cricketer");
+student1.introduce();
